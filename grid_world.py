e618663692fc612a88a68f18f5671e664227aa3c
Oleksandr Pimenov <pialgi@live.com>
2019-01-13 21:12:16 -0500

    iterative policy evaluation implemented

--- grid_world.py
+++ grid_world.py
@@ -1,7 +1,7 @@
 # Author Alex Pimenov Jan 2019
 # developed as a part of Reinforcement Learning learning
 # BLOG  https://programmingbee.net/
-# TODO: add github repo here
+# SOURCE https://github.com/Opimenov/Reinforcement_Learning/blob/master/grid_world.py
 # python3.6 make sure to run sudo pip install -U future
 # simple grid world to practice Reinforcement Learning 
 # dynamic programming techniques
@@ -21,7 +21,8 @@ class Grid_World(object): #environment
     terminal_states - list of terminal states
     current_state - state of the agent
     rewards - dict from state tuple to scalar reward
-    actions - dict from state tuple to available list of
+    all_actions - list of all available actions
+    state_actions - dict from state tuple to available list of
               actions for that state
     action_results - dict from (state,action) tuple to resulting state
     TODO:: change action result to return a list of tuples
@@ -30,7 +31,7 @@ class Grid_World(object): #environment
                            of this action being taken by the agent.
                            Could say, that this is a policy.
     '''
-
+    
     def __init__(self, rows, cols):
         ''' args:
         rows - number of rows in the grid
@@ -52,7 +53,8 @@ class Grid_World(object): #environment
         self.cols = cols
         self.states = []
         self.rewards = {}
-        self.actions = {}
+        self.all_actions = []
+        self.state_actions = {}
         self.action_result = {}
         self.action_probabilities = {}
         self.start_state = (0,0)
@@ -120,7 +122,7 @@ class Grid_World(object): #environment
         return state in self.current_state
 
     def show_grid(self):
-        print("\t#####STATES#####")
+        print("#####STATES#####")
         for row in range(self.rows):
             for col in range(self.cols):
                 print("+------",end="")
@@ -131,9 +133,14 @@ class Grid_World(object): #environment
         for col in range(self.cols):
             print("+------",end="")
         print("+")
+        print("STARTING STATE {0}".format(self.start_state))
+        print("TERMINAL STATES : ",end="")
+        for state in self.terminal_states:
+            print(state,end="  ")
+        print("")
 
     def show_rewards(self):
-        print("\t#####REWARDS#####")
+        print("#####REWARDS#####")
         for row in range(self.rows):
             for col in range(self.cols):
                 print("+---",end="")
@@ -144,24 +151,224 @@ class Grid_World(object): #environment
         for col in range(self.cols):
             print("+---",end="")
         print("+")
-        
-        
 
-    def init_simple_grid(self):
-        self.rows = 4
-        self.cols = 4
-        self.states = []
-        for i in range(self.rows):
-            for j in range(self.cols):
-                self.states.append((i,j))
-        self.discount = 1
-        self.start_state = (0,0)
-        self.terminal_states.append((self.rows-1,self.cols-1))
-        #init all rewards except terminal state to be -1
-        for state in self.states:
-            if state in self.terminal_states:
-                self.rewards[state] = 0
-            else:
-                self.rewards[state] = -1
-    
+    def str_list(self, list):
+        s = ""
+        for t in list: s = s+t
+        return s
+        
+    def show_actions(self):
+        print("#####ACTIONS#####")
+        for row in range(self.rows):
+            for col in range(self.cols):
+                print("+----",end="")
+            print("+\n|",end="")
+            for col in range(self.cols):
+                print("{:>4}|".format(self.str_list(self.state_actions[(row,col)])),end="")
+            print("")
+        for col in range(self.cols):
+            print("+----",end="")
+        print("+")
 
+    def str_dict(self,pair):
+        '''action probability pair'''
+        return ""+str(pair[0])+":"+str(pair[1])
+        
+    def show_policy(self):
+        print("#####POLICY#####")
+        for row in range(self.rows):
+            print("+-------------------------------------+")            
+            for col in range(self.cols):
+                act_list = self.state_actions[(row,col)]
+                print("state:"+self.str_dict((row,col)))
+                for a in act_list:
+                    print(" {:>4} ".format(\
+                        self.str_dict(\
+                                      (a,self.action_probabilities[(row,col),a])\
+                        )\
+                    ),end="")
+                print("")
+            print("")
+
+        
+    def show_env(self):
+        self.show_grid()
+        self.show_rewards()
+        self.show_actions()
+        self.show_policy()
+# end of Grid_World class
+
+def init_simple_grid():
+    g = Grid_World(4,4)
+    #init all states
+    g.discount = 1
+    #set starting and terminal states
+    g.start_state = (0,0)
+    g.terminal_states.append((g.rows-1,g.cols-1))
+    #init all rewards except terminal state to be -1
+    for state in g.states:
+        if state in g.terminal_states:
+            g.rewards[state] = 0
+        else:
+            g.rewards[state] = -1
+    #init all_actions
+    U = "\u2191"
+    R = "\u2192"
+    D = "\u2193"
+    L = "\u2190"
+    #init state_actions
+    g.all_actions = [U,R,D,L]
+    g.state_actions[(0,0)]=[R,D]
+    g.state_actions[(0,1)]=[R,D,L]
+    g.state_actions[(0,2)]=[R,D,L]
+    g.state_actions[(0,3)]=[D,L]
+    g.state_actions[(1,0)]=[U,R,D]
+    g.state_actions[(1,1)]=[U,R,D,L]
+    g.state_actions[(1,2)]=[U,R,D,L]
+    g.state_actions[(1,3)]=[U,D,L]
+    g.state_actions[(2,0)]=[U,R,D]
+    g.state_actions[(2,1)]=[U,R,D,L]
+    g.state_actions[(2,2)]=[U,R,D,L]
+    g.state_actions[(2,3)]=[U,D,L]
+    g.state_actions[(3,0)]=[U,R]
+    g.state_actions[(3,1)]=[U,R,L]
+    g.state_actions[(3,2)]=[U,R,L]
+    g.state_actions[(3,3)]=[]
+    #init action probabilities. The policy
+    #this is agly but easy to do and understand
+    #row 0
+    g.action_probabilities[((0,0),R)]=0.5
+    g.action_probabilities[((0,0),D)]=0.5
+    g.action_probabilities[((0,0),U)]=0
+    g.action_probabilities[((0,0),L)]=0
+    g.action_probabilities[((0,1),R)]=0.33
+    g.action_probabilities[((0,1),D)]=0.33
+    g.action_probabilities[((0,1),L)]=0.33
+    g.action_probabilities[((0,2),R)]=0.33
+    g.action_probabilities[((0,2),D)]=0.33
+    g.action_probabilities[((0,2),L)]=0.33
+    g.action_probabilities[((0,3),D)]=0.5
+    g.action_probabilities[((0,3),L)]=0.5
+    g.action_probabilities[((0,3),U)]=0
+    g.action_probabilities[((0,3),R)]=0   
+    #row 1
+    g.action_probabilities[((1,0),L)]=0    
+    g.action_probabilities[((1,0),U)]=0.33
+    g.action_probabilities[((1,0),R)]=0.33
+    g.action_probabilities[((1,0),D)]=0.33
+    g.action_probabilities[((1,1),U)]=0.25
+    g.action_probabilities[((1,1),R)]=0.25
+    g.action_probabilities[((1,1),D)]=0.25
+    g.action_probabilities[((1,1),L)]=0.25
+    g.action_probabilities[((1,2),U)]=0.25
+    g.action_probabilities[((1,2),R)]=0.25
+    g.action_probabilities[((1,2),D)]=0.25
+    g.action_probabilities[((1,2),L)]=0.25
+    g.action_probabilities[((1,3),U)]=0.33
+    g.action_probabilities[((1,3),D)]=0.33
+    g.action_probabilities[((1,3),L)]=0.33
+    g.action_probabilities[((1,3),R)]=0    
+    #row 2
+    g.action_probabilities[((2,0),L)]=0    
+    g.action_probabilities[((2,0),U)]=0.33
+    g.action_probabilities[((2,0),R)]=0.33
+    g.action_probabilities[((2,0),D)]=0.33
+    g.action_probabilities[((2,1),U)]=0.25
+    g.action_probabilities[((2,1),R)]=0.25
+    g.action_probabilities[((2,1),D)]=0.25
+    g.action_probabilities[((2,1),L)]=0.25
+    g.action_probabilities[((2,2),U)]=0.25
+    g.action_probabilities[((2,2),R)]=0.25
+    g.action_probabilities[((2,2),D)]=0.25
+    g.action_probabilities[((2,2),L)]=0.25
+    g.action_probabilities[((2,3),U)]=0.33
+    g.action_probabilities[((2,3),D)]=0.33
+    g.action_probabilities[((2,3),L)]=0.33
+    g.action_probabilities[((2,3),R)]=0    
+    #row 3
+    g.action_probabilities[((3,0),L)]=0
+    g.action_probabilities[((3,0),D)]=0    
+    g.action_probabilities[((3,0),U)]=0.5
+    g.action_probabilities[((3,0),R)]=0.5
+    g.action_probabilities[((3,1),D)]=0        
+    g.action_probabilities[((3,1),U)]=0.33
+    g.action_probabilities[((3,1),R)]=0.33
+    g.action_probabilities[((3,1),L)]=0.33
+    g.action_probabilities[((3,2),D)]=0        
+    g.action_probabilities[((3,2),U)]=0.33
+    g.action_probabilities[((3,2),R)]=0.33
+    g.action_probabilities[((3,2),L)]=0.33
+    g.action_probabilities[((3,3),D)]=0        
+    g.action_probabilities[((3,3),U)]=0
+    g.action_probabilities[((3,3),R)]=0
+    g.action_probabilities[((3,3),L)]=0
+    #init action_result dict
+    #row 0
+    g.action_result[((0,0),R)]=(0,1)
+    g.action_result[((0,0),D)]=(1,0)
+    g.action_result[((0,0),U)]=(0,0)
+    g.action_result[((0,0),L)]=(0,0)
+    g.action_result[((0,1),R)]=(0,2)
+    g.action_result[((0,1),D)]=(1,1)
+    g.action_result[((0,1),L)]=(0,0)
+    g.action_result[((0,2),R)]=(0,3)
+    g.action_result[((0,2),D)]=(1,2)
+    g.action_result[((0,2),L)]=(0,1)
+    g.action_result[((0,3),D)]=(1,3)
+    g.action_result[((0,3),L)]=(0,2)
+    g.action_result[((0,3),U)]=(0,3)
+    g.action_result[((0,3),R)]=(0,3)
+    #row 1
+    g.action_result[((1,0),L)]=(1,0)
+    g.action_result[((1,0),U)]=(0,0)
+    g.action_result[((1,0),R)]=(1,1)
+    g.action_result[((1,0),D)]=(2,0)
+    g.action_result[((1,1),U)]=(0,1)
+    g.action_result[((1,1),R)]=(1,2)
+    g.action_result[((1,1),D)]=(2,1)
+    g.action_result[((1,1),L)]=(0,1)
+    g.action_result[((1,2),U)]=(0,2)
+    g.action_result[((1,2),R)]=(1,3)
+    g.action_result[((1,2),D)]=(2,2)
+    g.action_result[((1,2),L)]=(1,1)
+    g.action_result[((1,3),U)]=(0,3)
+    g.action_result[((1,3),D)]=(2,3)
+    g.action_result[((1,3),L)]=(1,2)
+    g.action_result[((1,3),R)]=(1,3)
+    #row 2
+    g.action_result[((2,0),L)]=(2,0)
+    g.action_result[((2,0),U)]=(1,0)
+    g.action_result[((2,0),R)]=(2,1)
+    g.action_result[((2,0),D)]=(3,0)
+    g.action_result[((2,1),U)]=(1,1)
+    g.action_result[((2,1),R)]=(2,2)
+    g.action_result[((2,1),D)]=(3,1)
+    g.action_result[((2,1),L)]=(2,2)
+    g.action_result[((2,2),U)]=(1,2)
+    g.action_result[((2,2),R)]=(2,3)
+    g.action_result[((2,2),D)]=(3,2)
+    g.action_result[((2,2),L)]=(2,1)
+    g.action_result[((2,3),U)]=(1,3)
+    g.action_result[((2,3),D)]=(3,3)
+    g.action_result[((2,3),L)]=(2,2)
+    g.action_result[((2,3),R)]=(2,3)
+    #row 3
+    g.action_result[((3,0),L)]=(3,0)
+    g.action_result[((3,0),D)]=(3,0)    
+    g.action_result[((3,0),U)]=(2,0)
+    g.action_result[((3,0),R)]=(3,1)
+    g.action_result[((3,1),D)]=(3,1)     
+    g.action_result[((3,1),U)]=(2,1)
+    g.action_result[((3,1),R)]=(3,2)
+    g.action_result[((3,1),L)]=(3,0)
+    g.action_result[((3,2),D)]=(3,2)        
+    g.action_result[((3,2),U)]=(2,2)
+    g.action_result[((3,2),R)]=(3,3)
+    g.action_result[((3,2),L)]=(3,1)
+    g.action_result[((3,3),D)]=(3,3)
+    g.action_result[((3,3),U)]=(3,3)
+    g.action_result[((3,3),R)]=(3,3)
+    g.action_result[((3,3),L)]=(3,3)
+
+    g.show_env()
+    return g
--- iterative_policy_evaluation.py
+++ iterative_policy_evaluation.py
@@ -1,12 +1,31 @@
-import numpy as np
+# Author Alex Pimenov Jan 2019
+# developed as a part of Reinforcement Learning learning
+# BLOG  https://programmingbee.net/
+# SOURCE https://github.com/Opimenov/Reinforcement_Learning/blob/master/iterative_policy_evaluation.py
+# python3.6 make sure to run sudo pip install -U future
+# Iterative Policy Evaluation algorithm
+from grid_world import init_simple_grid
 
-def evaluate_policy_iteratively(num_of_states,
-                                actions,
-                                transition_matrix,
-                                rewards,
-                                discount,
-                                policy,
-                                threshold):
+def show_values(values, grid, iteration):
+    '''values is a dict from (row,col) to scalar value'''
+    print("#####VALUES##### iteration {0}".format(iteration))
+    for row in range(grid.rows):
+        for col in range(grid.cols):
+            print("+------",end="")
+        print("+\n|",end="")
+        for col in range(grid.cols):
+            value = values[(row,col)]
+            if value >= 0:
+                print(" {:>5.2f}|".format(value),end="")
+            else:
+                print("{:>5.2f}|".format(value),end="")
+        print("")
+    for col in range(grid.cols):
+        print("+------",end="")
+    print("+")
+    
+
+def run_algorithm():
     '''
     num_of_states - number of available states
     actions - matrix where col num represents a state, row represents action, 
@@ -21,16 +40,41 @@ def evaluate_policy_iteratively(num_of_states,
               num of states x num of actions. Each row contains probabilities
               of taking some action while being in row state
     '''
-    #initialize an array V(s)=0, for all states
-    values = np.zeros(num_of_states)
-    #Repeat
+    grid = init_simple_grid()
+    states = grid.states
+    gamma = grid.discount
+    rewards = grid.rewards
+    actions = grid.state_actions
+    action_result = grid.action_result
+    action_prob = grid.action_probabilities
+    threshold = 0.00000000001
+    #initialize dictionary of state to values 
+    values = {}
+    #set initial values to be zeros
+    for s in states: values[s] = 0
+    show_values(values,grid, 0)
+    #Repeat until convergence
+    iteration = 0
     while True:
-        biggest_delta = 0
-        for state in range(1,num_of_states+1):
+        delta = 0
+        for state in states:
             old_value = values[state]
-            #for each action
-            for probability in policy[:,state]:
-                values[state] += rewards[state] + dicount *    # sum of entries( row of policy[state]  * 
-        
-evaluate_policy_iteratively(5,1,1,1,1)                      
-
+            #exclude terminal states.
+            #there are no actions defined for terminal state
+            if len(grid.state_actions[state])!=0: 
+                weighted_sum = 0
+                #accumulate weighted with probabilities values of possible next states
+                for action in actions[state]:
+                    prob = action_prob[(state,action)]
+                    next_state_value = values[action_result[(state,action)]]
+                    weighted_sum = weighted_sum + (prob * next_state_value)
+                new_value = rewards[state] + gamma * weighted_sum
+                values[state] = new_value
+                delta = max(delta, abs(old_value - new_value))
+        iteration = iteration + 1
+        show_values(values,grid,iteration)
+        if delta < threshold:
+            print("policy evaluated")
+            break
+#    return values
+    
