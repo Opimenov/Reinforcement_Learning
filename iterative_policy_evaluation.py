45a2c15e071200869a530228a9766b09da5543b3
Afzal Pawaskar <afzal_pawaskar@dell.com>
2019-01-14 11:44:48 +0530

    added main and cleaned up

--- iterative_policy_evaluation.py
+++ iterative_policy_evaluation.py
@@ -19,27 +19,30 @@
 
 from grid_world import init_simple_grid
 
+
 def show_values(values, grid, iteration):
-    '''values is a dict from (row,col) to scalar value'''
+    """
+    values is a dict from (row,col) to scalar value
+    """
     print("#####VALUES##### iteration {0}".format(iteration))
     for row in range(grid.rows):
         for col in range(grid.cols):
-            print("+------",end="")
-        print("+\n|",end="")
+            print("+------", end="")
+        print("+\n|", end="")
         for col in range(grid.cols):
-            value = values[(row,col)]
+            value = values[(row, col)]
             if value >= 0:
-                print("{:>6.2f}|".format(value),end="")
+                print("{:>6.2f}|".format(value), end="")
             else:
-                print("{:>6.2f}|".format(value),end="")
+                print("{:>6.2f}|".format(value), end="")
         print("")
     for col in range(grid.cols):
-        print("+------",end="")
+        print("+------", end="")
     print("+")
     
 
 def run_algorithm(show_all_iterations=False):
-    '''
+    """
     num_of_states - number of available states
     actions - matrix where col num represents a state, row represents action, 
               element at action,state is the resulting state 
@@ -52,7 +55,8 @@ def run_algorithm(show_all_iterations=False):
     policy - matrix probability distribution of actions over states
               num of states x num of actions. Each row contains probabilities
               of taking some action while being in row state
-    '''
+    """
+
     grid = init_simple_grid()
     states = grid.states
     gamma = grid.discount
@@ -61,37 +65,43 @@ def run_algorithm(show_all_iterations=False):
     action_result = grid.action_result
     action_prob = grid.action_probabilities
     threshold = 0.000000000000000001
-    #initialize dictionary of state to values 
+    # initialize dictionary of state to values
     values = {}
-    #set initial values to be zeros
-    for s in states: values[s] = 0
-    #Repeat until convergence
+    # set initial values to be zeros
+    for s in states:
+        values[s] = 0
+    # Repeat until convergence
     iteration = 0
     while True:
-        if show_all_iterations: show_values(values,grid, 0)
+        if show_all_iterations:
+            show_values(values, grid, 0)
         new_values = {}
         delta = 0
         for state in states:
             old_value = values[state]
-            #exclude terminal states.
-            #there are no actions defined for terminal state
-            if len(grid.state_actions[state])!=0: 
+            # exclude terminal states.
+            # there are no actions defined for terminal state
+            if len(grid.state_actions[state]) != 0:
                 weighted_sum = 0
-                #accumulate weighted with probabilities values of possible next states
+                # accumulate weighted with probabilities values of possible next states
                 for action in actions[state]:
-                    prob = action_prob[(state,action)]
-                    next_state_value = values[action_result[(state,action)]]
+                    prob = action_prob[(state, action)]
+                    next_state_value = values[action_result[(state, action)]]
                     weighted_sum = weighted_sum + (prob * next_state_value)
                 new_value = rewards[state] + gamma * weighted_sum
                 new_values[state] = new_value
                 delta = max(delta, abs(old_value - new_value))
         values = new_values
-        values[(3,3)]=0 # not part of the algorithm but I set up thigns this way, too lazy to fix
+        values[(3, 3)] = 0  # not part of the algorithm but I set up things this way, too lazy to fix
         iteration = iteration + 1
-        if show_all_iterations: show_values(values,grid,iteration)
+        if show_all_iterations:
+            show_values(values, grid, iteration)
         if delta < threshold:
             print("policy evaluated")
-            show_values(values,grid,iteration)
+            show_values(values, grid, iteration)
             break
 #    return values
-    
+
+
+if __name__ == '__main__':
+    run_algorithm(show_all_iterations=True)
